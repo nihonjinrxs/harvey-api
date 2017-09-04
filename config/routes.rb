3ff8571d5d5d9c9077b4b3c12813d89a11712693
Ryan B. Harvey <ryan.b.harvey@ted.com>
2017-09-04 15:19:36 -0500

    rails g graphql:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
+
+  post "/graphql", to: "graphql#execute"
   devise_for :users
 
   resources :users, only: [:index, :show, :update]
